639ae8bfe3ec269517b90b61846d0904b16addb3
unknown <lmarquez@Desarrollo10.ms.local>
2015-09-30 10:30:40 -0300

    a

--- config/env/production.js
+++ config/env/production.js
@@ -1,3 +1,3 @@
 module.exports = {
-  db: 'mongodb://lmarquez:issabib12@ds051833.mongolab.com:51833/carpooling-tracker'
+  db: 'mongodb://heroku-user:1qaz2wsx@ds051833.mongolab.com:51833/carpooling-tracker'
 }
