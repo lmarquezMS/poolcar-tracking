dfca0f8b40a585dd1f222dab3024766aa39a8ee0
unknown <lmarquez@Desarrollo10.ms.local>
2015-09-30 11:00:24 -0300

    testing deploy

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV  = process.env.NODE_ENV || 'development';
+process.env.PORT      = process.env.PORT || 3000;
+
 console.log(process.env.NODE_ENV);
-// var mongoose  = require('./config/mongoose'),
-var  express     = require('./config/express')
-// var db  = mongoose();
+ var mongoose  = require('./config/mongoose'),
+  express     = require('./config/express');
+var db  = mongoose();
 var app = express();
 
 
-app.listen(3000);
+app.listen(process.env.PORT);
 
 console.log("Corriendo el servidor en http://localhost:3000/");
 
