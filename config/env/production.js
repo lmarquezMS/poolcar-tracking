e79fbfa1effe8c4c8c9caba4387155ec8da46675
unknown <lmarquez@Desarrollo10.ms.local>
2015-12-16 13:08:14 -0300

    arreglado direccion de retorno en login usando google para produccion

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,5 @@
 module.exports = {
   db: 'mongodb://heroku-user:1qaz2wsx@ds051833.mongolab.com:51833/carpooling-tracker',
-  sessionSecret: 'developmentSessionSecret'
+  sessionSecret: 'developmentSessionSecret',
   url: 'http://poolcar-tracking.herokuapp.com'
 }
